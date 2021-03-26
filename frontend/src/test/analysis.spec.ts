4e2ce03d694c847d7509dd66f38b71308f54d530
Zainrax <patrickb321@gmail.com>
2021-03-26 16:26:16 +1300

    test(analysis): add unit tests & profiling
    The FLIR cameras run at 9hz frame rate so we can determine the time it takes from first detection to measured.

--- frontend/src/test/analysis.spec.ts
+++ frontend/src/test/analysis.spec.ts
@@ -1,41 +1,76 @@
-import {processFile, isCPTV, getMeasuredFrames} from "./helpers";
+import {processFile, isCPTV, getMeasuredFrames, getSequenceOfScreeningState} from "./helpers";
 import {expect} from "chai";
 import {AnalysisResult} from "./tko-processing/tko_processing";
 import {ScreeningState, getScreeningState} from "../../src/types";
 
-const PersonNoMeasure = "20210322-112850.cptv";
+const PersonShouldMeasure = "20210322-112850.cptv";
+const PersonShouldNotMeasure = "20210326-110819.cptv";
 const PersonMeasure = "20210325-091249.cptv";
-const fileFiles = `${process.cwd()}/src/test/test_files`;
-const testFile = PersonMeasure;
+const EmptyShouldNotMeasure = "20210326-091927.cptv"
+const testFiles = `${process.cwd()}/src/test/test_files`;
+const processTestFile = (testFile: string): Promise<AnalysisResult[]> => processFile(`${testFiles}/${testFile}`);
 let result: AnalysisResult[];
 
-describe("Analyse cptv file using processing algorithm", async () => {
-  before(async () => {
-    result = await processFile(`${fileFiles}/${testFile}`);
-  });
+
+describe("Analyse cptv file using processing algorithm", () => {
   it("checks file is cptv", () => {
     const exampleMP4file = "1234.mp4";
-    expect(isCPTV(testFile)).to.be.true;
+    expect(isCPTV(PersonMeasure)).to.be.true;
     expect(isCPTV(exampleMP4file)).to.be.false;
   });
-  it("can process a file", () => {
-    expect(result, `${result[0]}`).have.length.above(0);
-  });
-  it("can find a body", () => {
-    expect(
-      result.filter(val => val.has_body),
-      "Found body"
-    ).have.length.above(0);
-  });
-  it("can measure temprature", () => {
-    const measuredTemps = getMeasuredFrames(result);
-    result.map(res => console.log(getScreeningState(res.next_state)));
-    expect(measuredTemps).have.length.above(0);
-  });
-  it("can get stable lock", () => {
-    const faceLocked = result.filter(
-      res => ScreeningState.STABLE_LOCK === getScreeningState(res.next_state)
-    );
-    expect(faceLocked).to.have.length.above(0);
-  });
+  describe("Measure Person's temprature", () => {
+    before(async () => {
+      result = await processTestFile(PersonMeasure)
+    });
+    it("can process a file", () => {
+      expect(result, `${result[0]}`).have.length.above(0);
+    });
+    it("can find a body", () => {
+      expect(
+        result.filter(val => val.has_body),
+        "Found body"
+      ).have.length.above(0);
+    });
+    it("can measure temprature", () => {
+      expect(
+        getSequenceOfScreeningState(result)
+      ).to.contain(ScreeningState.MEASURED)
+    });
+    it("can get stable lock", () => {
+      expect(
+        getSequenceOfScreeningState(result)
+      ).to.contain(ScreeningState.LARGE_BODY)
+    });
+    it("can find thermal reference", () => {
+      const thermalRef = result.filter(res => res.thermal_ref.temp > 0)
+      expect(thermalRef).to.have.length.above(0)
+    })
+  })
+
+  describe("Do not measure far person's temprature", () => {
+    before(async () => {
+      result = await processTestFile(PersonShouldNotMeasure)
+    })
+    it("can find a body", () => {
+      expect(
+        getSequenceOfScreeningState(result)
+      ).to.contain(ScreeningState.LARGE_BODY)
+    })
+    it("should not measure temprature", () => {
+      expect(
+        getSequenceOfScreeningState(result)
+      ).to.not.contain(ScreeningState.MEASURED)
+    })
+  })
+
+  describe("Do not measure empty video", () => {
+    before(async () => {
+      result = await processTestFile(EmptyShouldNotMeasure)
+    })
+    it("should not change screening states from ready", () => {
+      expect(getSequenceOfScreeningState(result)
+        .filter(state => state !== ScreeningState.READY && state !== ScreeningState.MISSING_THERMAL_REF))
+        .to.be.empty
+    })
+  })
 });
--- frontend/src/test/helpers.ts
+++ frontend/src/test/helpers.ts
@@ -1,11 +1,12 @@
-import { readFile } from "fs/promises";
+import {readFile} from "fs/promises";
 import * as cptvPlayer from "./cptv_player/cptv_player";
 import {
   initialize,
   analyse,
   AnalysisResult
 } from "./tko-processing/tko_processing";
-import { ScreeningState, getScreeningState } from "../types";
+import {ScreeningState, getScreeningState} from "../types";
+import {performance, PerformanceObserver} from "perf_hooks"
 
 const frameRes: [number, number] = [120, 160];
 
@@ -15,7 +16,6 @@ const checkExt = (ext: string) => (file: string) =>
     .pop()
     ?.toLowerCase() === ext.toLowerCase();
 let hasInit = false;
-
 export const isCPTV = (file: string) => checkExt("CPTV")(file);
 
 const getFrame = () => {
@@ -23,32 +23,66 @@ const getFrame = () => {
   const frameBuffer = new Uint8Array(arrBuffer);
   const frameInfo = cptvPlayer.getRawFrame(frameBuffer);
   const frame = new Uint16Array(arrBuffer);
-  console.log(frame);
-  return { frame, frameInfo };
+  return {frame, frameInfo};
 };
 
-export const processFile = async (file: string) => {
+export const processFile = async (file: string): Promise<AnalysisResult[]> => {
+
+  const obs = new PerformanceObserver((list, obs) => {
+    const analysisTimes = list.getEntriesByName('Start Analysis to Finish Analysis')
+    const fileAnalysisTime = list.getEntriesByName('Start File to Finish File')
+
+
+    const averageAnalysisTime = analysisTimes.map(entry => entry.duration).reduce((total, next) => total + next, 0) / analysisTimes.length
+    console.log(`\tAverage Time to analyse frame: ${averageAnalysisTime}ms`)
+    console.log(`\tTotal Time to Finish: ${fileAnalysisTime[0].duration}ms`)
+    console.log(`\tTotal Amount of Frames: ${analysisTimes.length}`)
+    obs.disconnect()
+  })
+
+  obs.observe({entryTypes: ['measure'], buffered: true})
+
   const result = [];
+  let startMeasureFrame = 0
+  let finishMeasureFrame = 0
+  performance.mark('Start File')
   if (isCPTV(file)) {
     let hasNextFrame = true;
     const fileBytes = await readFile(file);
     let firstFrame = true;
     if (!hasInit) {
       initialize(frameRes[0], frameRes[1]);
-      cptvPlayer.initWithCptvData(fileBytes);
       hasInit = true;
     }
+    cptvPlayer.initWithCptvData(fileBytes);
 
     do {
       const frame = getFrame();
 
-      const { frame_number } = frame.frameInfo;
+      const {frame_number} = frame.frameInfo;
       hasNextFrame = frame_number !== 0 || firstFrame;
       firstFrame = false;
+      performance.mark('Start Analysis')
       const analysis = analyse(frame.frame, 36.5, 6000);
+      performance.mark('Finish Analysis')
+      performance.measure('Start Analysis to Finish Analysis', 'Start Analysis', 'Finish Analysis')
+      startMeasureFrame = startMeasureFrame === 0 &&
+        getScreeningState(analysis.next_state) !== ScreeningState.READY ?
+        frame_number :
+        startMeasureFrame
+      finishMeasureFrame = finishMeasureFrame === 0 &&
+        getScreeningState(analysis.next_state) === ScreeningState.MEASURED ?
+        frame_number :
+        finishMeasureFrame
       result.push(analysis);
     } while (hasNextFrame);
   }
+  performance.mark('Finish File')
+  performance.measure('Start File to Finish File', 'Start File', 'Finish File')
+  const cameraFrameRate = 9
+  let totalFramesToMeasrue = finishMeasureFrame - startMeasureFrame
+  totalFramesToMeasrue = totalFramesToMeasrue > 0 ? totalFramesToMeasrue : 0
+  console.log(`\tTotal from Start to Measure: ${totalFramesToMeasrue} frames, ${totalFramesToMeasrue / cameraFrameRate}`)
   return result;
 };
 
@@ -60,3 +94,8 @@ export const getMeasuredFrames = (results: AnalysisResult[]) => {
   );
   return measurements;
 };
+
+export const getSequenceOfScreeningState = (results: AnalysisResult[]) => {
+  const ScreeningStates = results.filter((val, index, arr) => val.next_state !== arr[index - 1]?.next_state)
+  return ScreeningStates.map(val => getScreeningState(val.next_state))
+}
