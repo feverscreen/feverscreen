5e804bce3853f1632e6793c0de4864e022a60880
Zainrax <patrickb321@gmail.com>
2021-03-30 09:41:23 +1300

    feat(processing): update shell build script

--- processing/build.sh
+++ processing/build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 wasm-pack build --target no-modules --out-dir pkg-no-modules
-cp ./pkg-no-modules/smooth_bg.wasm ../feverscreen/frontend/public
-cp ./pkg-no-modules/package.json ../feverscreen/frontend/smooth
-cp ./pkg-no-modules/*.ts ../feverscreen/frontend/smooth
-echo "export default wasm_bindgen;" >> ./pkg-no-modules/smooth.js
-cp ./pkg-no-modules/*.js ../feverscreen/frontend/smooth
+cp ./pkg-no-modules/tko_processing_bg.wasm ../frontend/public
+cp ./pkg-no-modules/*.ts ../frontend/processing
+cp ./pkg-no-modules/package.json ../frontend/processing
+echo "export default wasm_bindgen;" >> ./pkg-no-modules/tko_processing.js
+cp ./pkg-no-modules/*.js ../frontend/processing
 
-wasm-pack build --target web --out-dir pkg-web
-cp ./pkg-web/*.* ../feverscreen.github.io/smooth
+# wasm-pack build --target web --out-dir pkg-web -- --features=perf-profiling,output-mask-shapes,face-thresholding
+# cp ./pkg-web/*.* ../feverscreen.github.io/processing
 
 wasm-pack build --target nodejs --out-dir pkg-node
-cp ./pkg-node/*.* ../feverscreen/frontend/src/tests/smooth
+cp ./pkg-node/*.* ../frontend/src/test/tko-processing/
