4860a7e654575e1f90e6994565565ca1f43ba176
Zainrax <patrickb321@gmail.com>
2021-04-13 10:55:11 +1200

    test: add real temps matching with recordings for profile files

--- frontend/src/test/TestFiles.ts
+++ frontend/src/test/TestFiles.ts
@@ -5,10 +5,21 @@ import {parse, unparse} from "papaparse"
 import argv from "minimist"
 
 
-// Download csv from https://docs.google.com/spreadsheets/d/1Mj58ppbCQTw5OgcSks7Op6nxj4Rdw259uhBPQkUaO0Y or similar file.
-// Processes files given csv containing URL, Scanned("Amount of people that attempted scanning"), and Feature
-const CACOPHONY_API = "https://api.cacophony.org.nz"
-const TEST_FILE_DIR = `${__dirname}/test_files/`
+interface ScanItem {
+  URL: string;
+  Scanned: string; // Amount of People that tried to scan
+  Feature: Features[];
+}
+
+interface TempResult {
+  Device: string;
+  "Screened Temp C": string;
+  "Time": string;
+}
+
+type Obj = {
+  [index: string]: any
+}
 
 enum Features {
   LongTime,
@@ -22,14 +33,33 @@ enum Features {
   StandingInFront
 }
 
-interface ScanItem {
-  URL: string;
-  Scanned: string; // Amount of People that tried to scan
-  Feature: Features[];
+
+// Download csv from https://docs.google.com/spreadsheets/d/1Mj58ppbCQTw5OgcSks7Op6nxj4Rdw259uhBPQkUaO0Y or similar file.
+// Processes files given csv containing URL, Scanned("Amount of people that attempted scanning"), and Feature
+const CACOPHONY_API = "https://api.cacophony.org.nz"
+const TEST_FILE_DIR = `${__dirname}/test_files/`
+
+const SaltDevices: any = {
+  TKO8: "fs-1220",
+  TKO11: "fs-1228",
+  TKO13: "fs-1217",
+  TKO16: "fs-1216",
+  "manually-moral-bonefish": "fs-1204"
 }
 
-type Obj = {
-  [index: string]: any
+const months: Obj = {
+  'Jan': 0,
+  'Feb': 1,
+  'Mar': 2,
+  'Apr': 3,
+  'May': 4,
+  'Jun': 5,
+  'Jul': 6,
+  'Aug': 7,
+  'Sep': 8,
+  'Oct': 9,
+  'Nov': 10,
+  'Dec': 11
 }
 
 const checkExt = (ext: string) => (file: string) =>
@@ -127,7 +157,8 @@ async function writeRecordings(user: string, pass: string, records: ScanItem[]) 
         return {id: ID, date: recordingDateTime, device: Device.devicename, duration, recordingToken: recordingInfo.downloadRawJWT, ...rest}
       }).map(async recordingInfo => {
         const info = await recordingInfo;
-        const {id, date, device, recordingToken} = info
+        const {id, date, recordingToken} = info
+        const device = info.device in SaltDevices ? SaltDevices[info.device as string] : info.device as string
         const fileName = createFileName(id, device, new Date(date))
         const isDuplicate = await checkIsDuplicate(fileName)
         if (!isDuplicate) {
@@ -142,8 +173,16 @@ async function writeRecordings(user: string, pass: string, records: ScanItem[]) 
         } else {
           console.log(`Skip writing ${TEST_FILE_DIR}${fileName} as already exists...`)
         }
-        return {fileName, ...info}
+        return {fileName, saltDevice: device, ...info}
       }).filter(info => info))
+}
+
+const parseReadingDates = (dateStr: string) => {
+  const [time, date] = dateStr.split("-")
+  const [hours, minutes, seconds]: any = time.split(":")
+  const [month, day, year]: any = date.split(" ").slice(2)
+  const parsedDate = new Date(year, months[month], day, hours, minutes, seconds)
+  return parsedDate
 }
 
 async function run() {
@@ -158,35 +197,68 @@ async function run() {
     console.error("File must be a valid csv file")
     return
   }
-  var rl = readline.createInterface({
+  let rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
     terminal: false
   });
-  try {
-    const user = args.u
-    console.log(`Please input password for ${user}:`)
-    const line = rl[Symbol.asyncIterator]();
-    const lineVal = await line.next()
-    const pass = lineVal.value as string
-    const file = await readFile(args.f, 'utf8')
-    console.log(`Reading ${args.f} to get Test Data...`)
 
-    const records: ScanItem[] = parse<ScanItem>(file, {
-      skipEmptyLines: true,
-      header: true
-    }).data.filter((obj: ScanItem) => obj.Scanned !== "" && obj.Scanned !== "0")
+  const user = args.u
+  console.log(`Please input password for ${user}:`)
+  const line = rl[Symbol.asyncIterator]();
+  const lineVal = await line.next()
+  const pass = lineVal.value as string
+  const file = await readFile(args.f, 'utf8')
+  console.log(`Reading ${args.f} to get Test Data...`)
 
-    const writtenRecordings = await writeRecordings(user, pass, records)
-    const keysToKeep = ["fileName", "id", "date", "device", "duration", "Scanned", "Feature", "Notes", "Start Time", "URL"]
-    const CSVRecords = reduceObjsToCertainKeys(writtenRecordings as object[], keysToKeep)
+  const records: ScanItem[] = parse<ScanItem>(file, {
+    skipEmptyLines: true,
+    header: true
+  }).data.filter((obj: ScanItem) => obj.Scanned !== "" && obj.Scanned !== "0")
 
-    const csv = unparse(CSVRecords)
-    console.log(`Writing Test Data Info to ${TEST_FILE_DIR}tko-test-files.csv with ${writtenRecordings.length} Records...`)
-    writeFile(`${TEST_FILE_DIR}tko-test-files.csv`, csv)
-  } catch (e) {
-    console.error(e)
-  }
+  const writtenRecordings: any = await writeRecordings(user, pass, records)
+
+  const TempReadingsFile = await readFile(`${process.cwd()}/temp-readings.csv`, 'utf8')
+  parse<TempResult>(TempReadingsFile, {
+    skipEmptyLines: true, header: true, complete: (res) => {
+      try {
+        const TempReading = res.data
+        const Temps: Obj = {}
+        TempReading.forEach(({Device, 'Screened Temp C': temp, 'Time': date}) => {
+          if (!(Device in Temps)) {
+            Temps[Device] = []
+          }
+          Temps[Device].push({temp: temp, date: parseReadingDates(date)})
+        })
+
+        const keysToKeep = ["fileName", "id", "Time", "Date", "Device", "duration", "Scanned", "Feature", "Notes", "Start Time", "URL"]
+        const CSVRecords = reduceObjsToCertainKeys(writtenRecordings, keysToKeep).map(recording => {
+          // Based on difference between video recording was made, and temp recorded.
+          const TimeLeeway = Math.max(recording.duration - 5.5, 1)
+          const [day, month, year] = recording.Date.split("/")
+          const [hours, minutes, seconds] = recording.Time.split(":")
+          const date = new Date(year, month - 1, day, hours, minutes, seconds)
+
+          recording.Device = recording.Device in SaltDevices ? SaltDevices[recording.Device] : recording.Device
+          const device = recording.Device
+          if (device in Temps) {
+            const realTemps = Temps[device].filter((temp: any) => {
+              const timeDiff = Math.abs(date.getTime() - temp.date.getTime()) / 1000
+              return timeDiff < TimeLeeway
+            }).map((res: any) => res.temp)
+            return {...recording, realTemps}
+          }
+          return {...recording, realTemps: []}
+        })
+
+        const csv = unparse(CSVRecords)
+        console.log(`Writing Test Data Info to ${TEST_FILE_DIR}tko-test-files.csv with ${writtenRecordings.length} Records...`)
+        writeFile(`${TEST_FILE_DIR}tko-test-files.csv`, csv)
+      } catch (e) {
+        console.error(e)
+      }
+    }
+  })
 }
 
 run()
