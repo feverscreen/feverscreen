27f78be8e2a95f5216780c790a93beb5a2670868
Zainrax <patrickb321@gmail.com>
2021-08-05 11:25:19 +1200

    refactor(vue): upgrade to next

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
+  experiments: {
+    syncWebAssembly: true
+  },
   publicPath: process.env.NODE_ENV === "production" ? "/static/dist/" : "",
   configureWebpack: config => {
     config.optimization = {
